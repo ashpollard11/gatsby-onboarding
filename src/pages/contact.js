c08180a0a096a1c2ff7a4926ae5945e944250dd3
Ashley Pollard <ash.pollard11@yahoo.com>
2020-02-17 19:18:55 -0500

    Creating nested layout components

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,15 @@
 import React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header"
+import Layout from "../components/layout";
 
 export default () => (
-  <div style={{ color: `teal` }}>
+  <Layout>
     <Link to="/">Home</Link>
     <Header headerText="Contact" />
-    <p>Send us a message!</p>
-  </div>
+    <p>Send us a message! I'd love to talk! Email me at the address below</p>
+    <p>
+      <a href="mailto:me@example.com">me@example.com</a>
+    </p>
+  </Layout>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,16 @@
 import React from "react"
 import { Link } from "gatsby"
+import Layout from "../components/layout"
 import Header from "../components/header"
 
 export default () => (
-  <div style={{ color: `purple` }}>
-    <Link to="/contact/">Contact</Link>
-    <Header headerText="Hello Gatsby!" />
-    <p>What a world.</p>
-    <img src="https://source.unsplash.com/random/400x200" alt="" />
-  </div>
+  <Layout>
+	<Link to="/contact/">Contact</Link>
+	<Header headerText="Hi! I'm building a fake Gatsby site as part of a tutorial!" />
+	<img src="https://source.unsplash.com/random/400x200" alt="" />
+	<p>
+		What do I like to do? Lots of course but definitely enjoy building
+		websites.
+	</p>
+  </Layout>
 )
