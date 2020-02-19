7df66acdf89fdbae1b144e3370bc25015ff96ee9
Ashley Pollard <ash.pollard11@yahoo.com>
2020-02-19 11:43:28 -0500

    Programmatically Create Pages from Data

--- src/pages/my-files.js
+++ src/pages/my-files.js
@@ -3,7 +3,7 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 
 export default ({ data }) => {
-  console.log(data)
+//   console.log(data)
   return (
     <Layout>
       <div>
